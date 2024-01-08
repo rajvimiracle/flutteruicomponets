ee8901e9fc309f5d1b9728780565e875da8fcc85
rajvimiracle <155509512+rajvimiracle@users.noreply.github.com>
2024-01-08 16:18:06 +0530

    add push

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
+import 'package:flutteruicomponets/custom_elements/elements/Customappbar.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,19 +9,20 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return FlutterSizer(
+      builder: (context, orientation, screenType) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+            title: 'Flutter Sizer Example',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: home());
+      },
     );
   }
 }
-
